bf9bc5aa00bcfb63d361fb09738a6603080f0f3b
VOST Portugal <hello@vost.pt>
2019-04-11 13:38:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -359,10 +359,6 @@ client.on('message', msg => {
 	// End 
 	
 	//Experimental
-	if (message.mentions.users.has(client.id(@559536284947972097))) {
-		msgString = `${msg.author}, só estou a interagir contigo porque me programaram para isso. Mas não fiques com ideias, ok?`
-		msg.channel.send(msgString)
-	}
 	
 	//End
 	if (msg.content === "!coffee") {
