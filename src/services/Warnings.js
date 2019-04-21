c8466ff8d5ad98d4c92bd8c6d16c049616dd60ec
Jonathan <jonathanserrario@gmail.com>
2019-04-21 03:18:47 +0100

    Fix bug

--- src/services/Warnings.js
+++ src/services/Warnings.js
@@ -36,9 +36,9 @@ const getAll = async () => {
 const getWarnings = async (client) => {
   const warnings = await getAll();
 
-  trataDados(warnings, acores);
-  trataDados(warnings, madeira);
-  trataDados(warnings, continente);
+  trataDados(warnings, "acores");
+  trataDados(warnings, "madeira");
+  trataDados(warnings, "continente");
 
 };
 
