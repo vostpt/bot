3e7b8452a2d59aae16fa96da902e4a89c4198a16
miguelcarreiro <miguel.carreiro97@gmail.com>
2019-04-23 21:11:07 +0100

    feat: Comments in ProCiv and Winds (from /services)

--- src/services/ProCiv.js
+++ src/services/ProCiv.js
@@ -8,24 +8,47 @@ const statuses = {
   vigilância: '9',
 };
 
+/**
+ * Get all occurrences
+ *
+ * @returns {Array} data
+ */
 const getAll = async () => {
   const { data = [] } = await ProcivApi.getAll();
 
   return data;
 };
 
+/**
+ * Get occurrence with a certain id
+ *
+ * @param {String} requestedId
+ * @returns {Array}
+ */
 const getById = async (requestedId) => {
   const events = await getAll();
 
   return events.filter(({ id }) => id === requestedId);
 };
 
+/**
+ * Get occurrences in a certain city
+ *
+ * @param {String} cityId
+ * @returns {Array}
+ */
 const getByCity = async (cityId) => {
   const events = await getAll();
 
   return events.filter(({ l: city }) => `#IF${city}` === cityId);
 };
 
+/**
+ * Get occurrences that have more than a given number of operatives
+ *
+ * @param {Number} amountOfMansInvolved If < 0 the function will return a Promise object that was rejected
+ * @returns {Promise|Array}
+ */
 const filterByMinimumMans = async (amountOfMansInvolved) => {
   if (amountOfMansInvolved < 0) {
     return Promise.reject();
@@ -36,6 +59,12 @@ const filterByMinimumMans = async (amountOfMansInvolved) => {
   return events.filter(({ o: mans }) => mans > amountOfMansInvolved);
 };
 
+/**
+ * Get occurrences that have more than a given number of ground assets
+ *
+ * @param {Number} amountOfCarsInvolved If < 0 the function will return a Promise object that was rejected
+ * @returns {Promise|Array}
+ */
 const filterByMinimumCars = async (amountOfCarsInvolved) => {
   if (amountOfCarsInvolved < 0) {
     return Promise.reject();
@@ -45,6 +74,12 @@ const filterByMinimumCars = async (amountOfCarsInvolved) => {
   return events.filter(({ t: cars }) => cars > amountOfCarsInvolved);
 };
 
+/**
+ * Get occurrences that have more than a given number of aerial assets
+ *
+ * @param {Number} amountOfAerialsInvolved If < 0 the function will return a Promise object that was rejected
+ * @returns {Promise|Array}
+ */
 const filterByMinimumAerials = async (amountOfAerialsInvolved) => {
   if (amountOfAerialsInvolved < 0) {
     return Promise.reject();
@@ -54,6 +89,12 @@ const filterByMinimumAerials = async (amountOfAerialsInvolved) => {
   return events.filter(({ h: aerials }) => aerials > amountOfAerialsInvolved);
 };
 
+/**
+ * Get occurrences that have a given status
+ *
+ * @param {String} requestedStatus
+ * @returns {Array}
+ */
 const filterByStatus = async (requestedStatus) => {
   const events = await getAll();
 
