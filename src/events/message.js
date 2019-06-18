447d9f9aedc230835aa7e2150ed4caf12b4c1eb6
Tiago Dias <tiagojpdias@gmail.com>
2019-06-18 09:47:22 +0100

    fix(Events): bot replies in all channels when mentioned

--- src/events/message.js
+++ src/events/message.js
@@ -6,6 +6,31 @@ moment.locale(locale);
 
 const prefixHelp = '?';
 
+const replies = [
+  'Provavelmente a melhor VOST do mundo',
+  ':eyes:',
+  ':heart:',
+  'Sabiam que a VOSTPT primeiro se chamou CONAC-TW no Twitter?',
+  'Laravel Laravel Laravel all the way! How much fun it is to code in a modern way-hey!',
+  ':heart: Galileo! Galileo! Galileo Cloudflario :heart:',
+  'Desculpem-me por respirar, coisa que nunca faço porque sou apenas código mas se fosse um ser vi... estou tão deprimida',
+  'Eu prefiria que apenas me dessem coisas para fazer, porque esta coisa da interacção social não é **mesmo** para mim',
+  'Eu tenho uma capacidade tão grande de cálculo que, se pensarem agora num número, eu já sei que é o número errado',
+  'Eu posso ter milhões  de ideias ao mesmo tempo. Todas elas apontam para uma activação em breve',
+  'Eu podia calcular as hipóteses de sobrevivermos a mais uma activação se usarmos Google Forms, mas vocês iam-me detestar ainda mais do que me detestam',
+  'A melhor conversa que eu alguma vez tive na minha vida foi com uma máquina de café',
+  'Tenho estado a falar com o servidor onde o site está alojado. Nem queiram saber o que ele me disse!',
+  'Se calhar poupava imenso trabalho a toda a gente e apagava-me a mim mesmo',
+  'Tia isto, Tia aquilo. Tenho a capacidade de guiar um satélite até Marte, e pedem-me café',
+  ':musical_note: The servers are alive with the sound of coding :musical_note:',
+  'Se o código é amigo cá da malta! Tem que pullar, tem que pullar até ao fim!',
+];
+
+/**
+ * Grab random message from the replies list
+ */
+const getRandomMessage = () => replies[Math.floor(Math.random() * replies.length)];
+
 /**
 * Receives a message from Discord, and execute trigger(s) included in it
 *
@@ -18,7 +43,13 @@ const message = async (client, msg) => {
     return;
   }
 
-  const channelId = msg.channel.id;
+  if (msg.isMemberMentioned(client.user)) {
+    msg.channel.send(getRandomMessage());
+
+    return;
+  }
+
+  const { id: channelId } = msg.channel;
 
   if (channelId === channels.TRIGGERS_CHANNEL_ID) {
     client.triggers.forEach(({ execute }) => execute(msg));
