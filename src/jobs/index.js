d0471155d9aaa488865352ee56c9a7526a26d64a
Tiago Dias <tiagojpdias@gmail.com>
2019-07-05 16:14:25 +0100

    feat(Earthquakes): check every 30s for worth mentioning earthquakes

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,19 +1,55 @@
 const schedule = require('node-schedule');
+const moment = require('moment');
 const { Fires, Earthquakes, Warnings } = require('../services');
 const { channels } = require('../../config/bot');
+
+/**
+ * Check if the earthquake level above threshold
+ *
+ * @param {String} event
+ * @param {Number} threshold
+ */
+const eventAboveThreshold = (event, threshold = 0) => {
+  const regexp = /\*\*(\d\.\d)\*\*/;
+
+  const matchs = event.match(regexp) || [];
+  const [, intensity = 0] = matchs;
+
+  return intensity >= threshold;
+};
+
+/**
+ * In memory Set to track already sent notifications
+ */
+const sentEarthquakesNotifications = new Set();
+/**
+ * Check if notification has already been sent
+ *
+ * @param {String} event
+ */
+const checkAlreadySent = event => !sentEarthquakesNotifications.has(event);
 
 class Jobs {
   constructor(client) {
     this.client = client;
   }
 
+  /**
+   * Start all jobs
+   */
   startAll() {
     this.forestFires();
     this.earthquakes();
+    this.noticeableEarthquakes({ threshold: 2.5 });
     this.warnings();
     this.fireRisk();
+
+    this.resetSentNotifications();
   }
 
+  /**
+   * Check for forest fires
+   */
   forestFires() {
     const rule = new schedule.RecurrenceRule();
 
@@ -23,6 +59,9 @@ class Jobs {
     schedule.scheduleJob(rule, () => Fires.getForestFires(this.client));
   }
 
+  /**
+   * Check for warnings
+   */
   warnings() {
     const rule = new schedule.RecurrenceRule();
 
@@ -31,6 +70,9 @@ class Jobs {
     schedule.scheduleJob(rule, () => Warnings.getWarnings(this.client));
   }
 
+  /**
+   * Check for fires probabilities for today
+   */
   fireRisk() {
     const rule = new schedule.RecurrenceRule();
 
@@ -49,18 +91,83 @@ class Jobs {
     });
   }
 
+  /**
+   * Checks for yesterday's earthquakes
+   */
   earthquakes() {
     const rule = new schedule.RecurrenceRule();
 
     rule.hour = 0;
     rule.minute = 0;
-    rule.second = 30;
+    rule.second = 10;
 
     try {
-      schedule.scheduleJob(rule, () => Earthquakes.getEarthquakes(this.client));
+      schedule.scheduleJob(rule, () => {
+        const yesterday = moment().subtract(1, 'days').format('L');
+        const { events, eventsSensed } = Earthquakes.getEarthquakes(yesterday);
+
+        if (eventsSensed.length > 0) {
+          this.client.channels.get(channels.EARTHQUAKES_CHANNEL_ID).send(`***Sismo(s) sentid(o) dia ${yesterday}:***\n${eventsSensed.join('\n')}`);
+        }
+
+        if (events.length > 0) {
+          this.client.channels.get(channels.EARTHQUAKES_CHANNEL_ID).send(`***Sismo(s) de ${yesterday}:***\n${events.join('\n')}`);
+        }
+      });
     } catch (e) {
       //
     }
+  }
+
+  /**
+   * Checks for noticeable earthquakes worthing immediate annoucement
+   *
+   * @param {Object} config
+   */
+  noticeableEarthquakes({ threshold } = {}) {
+    const rule = new schedule.RecurrenceRule();
+
+    rule.minute = new schedule.Range(1, 56, 5);
+    rule.second = 30;
+
+    try {
+      schedule.scheduleJob(rule, async () => {
+        const today = moment().format('L');
+        const { events, eventsSensed } = await Earthquakes.getEarthquakes(today);
+
+        const noticeableEvents = events
+          .filter(checkAlreadySent)
+          .filter(event => eventAboveThreshold(event, threshold));
+
+        const noticeableSensedEvents = eventsSensed
+          .filter(checkAlreadySent)
+          .filter(event => eventAboveThreshold(event, threshold));
+
+        if (noticeableSensedEvents.length > 0) {
+          this.client.channels.get(channels.EARTHQUAKES_CHANNEL_ID).send(`***Sismo(s) sentido(s) dia ${today}:***\n${noticeableSensedEvents.join('\n')}`);
+          noticeableSensedEvents.forEach(event => sentEarthquakesNotifications.add(event));
+        }
+
+        if (noticeableEvents.length > 0) {
+          this.client.channels.get(channels.EARTHQUAKES_CHANNEL_ID).send(`***Sismo(s) de ${today}:***\n${noticeableEvents.join('\n')}`);
+          noticeableEvents.forEach(event => sentEarthquakesNotifications.add(event));
+        }
+      });
+    } catch (e) {
+      //
+    }
+  }
+
+  /**
+   * Clear yesterday's notifications
+   */
+  static resetSentNotifications() {
+    const rule = new schedule.RecurrenceRule();
+
+    rule.hour = 0;
+    rule.minute = 0;
+
+    schedule.scheduleJob(rule, () => sentEarthquakesNotifications.clear());
   }
 }
 
