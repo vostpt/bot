f189ea123d8a816d97ee5bdd3060f638d0ade735
miguelcarreiro <miguel.carreiro97@gmail.com>
2019-07-21 11:16:04 +0100

    feat: create stats command
    Reply to Discord total number of means, discriminated by occurrence state

---
+++ src/commands/stats.js
@@ -0,0 +1,54 @@
+const {
+  Prociv,
+} = require('../services');
+const { cooldown } = require('../../config/bot');
+
+module.exports = {
+  name: 'stats',
+  args: true,
+  cooldown,
+  usage: `
+    **!stats** - *Mostra o número total de meios envolvidos nas ocorrências, descriminado por estado.*
+  `,
+  description: '',
+
+  /**
+  * Send to Discord stats about total number of means
+  *
+  * @async
+  * @param {Message} message
+  * @param {Array} args
+  */
+  async execute(message, args) {
+    if (this.args && args.length === 0) {
+      const occurrences = await Prociv.getAll();
+      const counters = {};
+      let totalCounters = [0, 0, 0, 0];
+
+      occurrences.forEach((element) => {
+        const {
+          o: mans,
+          t: cars,
+          a: helicopters,
+          ide: statusId,
+        } = element;
+
+        const elementValues = [1, mans, cars, helicopters];
+
+        totalCounters = totalCounters.map((val, index) => val + elementValues[index]);
+
+        if (!(statusId in counters)) {
+          counters[statusId] = elementValues;
+        } else {
+          counters[statusId] = counters[statusId].map((val, index) => val + elementValues[index]);
+        }
+      });
+
+      message.channel.send(`:fire: ***Situação operacional nacional:***\nValores totais: ${totalCounters[0]} :fire: | ${totalCounters[1]} :man_with_gua_pi_mao: | ${totalCounters[2]} :fire_engine: | ${totalCounters[3]} :helicopter:`);
+
+      Object.keys(counters).forEach((statusId) => {
+        message.channel.send(`Estado ${Prociv.statusIdToDesc[statusId]}: ${counters[statusId][0]} :fire: | ${counters[statusId][1]} :man_with_gua_pi_mao: | ${counters[statusId][2]} :fire_engine: | ${counters[statusId][3]} :helicopter:`);
+      });
+    }
+  },
+};
