8811824176a992ac3e12d0acda311f7a8061b488
Tiago Dias <tiagojpdias@gmail.com>
2019-08-09 16:46:08 +0100

    fix(scripts): documentation deploy script

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:vostpt/bot.git master:gh-pages
+git push -f https://github.com/vostpt/bot.git master:gh-pages
 
 cd -
