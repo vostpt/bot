39552c647e4532421e5e03db44df1156931455cc
miguelcarreiro <miguel.carreiro97@gmail.com>
2019-08-15 00:23:01 +0100

    [BOT-123] Add ArcGis dashboard screenshot to  Fuel Stats tweet images

--- src/api/Fuel.js
+++ src/api/Fuel.js
@@ -4,10 +4,13 @@ const fuelUrl = 'https://janaodaparaabastecer.vost.pt';
 
 const fuelStatsUrl = `${fuelUrl}/graphs/stats`;
 
+const esriFuelStatsUrl = 'https://www.arcgis.com/apps/opsdashboard/index.html#/a4890bfd60df4c07bd67c857b73b452f';
+
 const get = () => api.get(`${fuelUrl}/storage/data/stats_global.json`);
 
 module.exports = {
   fuelUrl,
   fuelStatsUrl,
+  esriFuelStatsUrl,
   get,
 };
