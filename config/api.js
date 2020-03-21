76fa6684861d16b87b390f7d0587b825ca953021
miguelcarreiro <miguelcarreiro@users.noreply.github.com>
2020-03-21 12:19:07 +0000

    fix: Simplify FAQ URL in config

--- config/api.js
+++ config/api.js
@@ -1,18 +1,20 @@
 const baseURL = 'https://bot-api.vost.pt';
 
+const baseFaqsURL = (id, gid) => `https://docs.google.com/spreadsheets/d/e/${id}/pub?gid=${gid}&single=true&output=csv`;
+
 const coronaFaqRefs = {
   id: process.env.CORONAFAQID,
   gid: process.env.CORONAFAQGID,
 };
 
-const coronaFaqsURL = `https://docs.google.com/spreadsheets/d/e/${coronaFaqRefs.id}/pub?gid=${coronaFaqRefs.gid}&single=true&output=csv`;
+const coronaFaqsURL = baseFaqsURL(coronaFaqRefs.id, coronaFaqRefs.gid);
 
 const govFaqRefs = {
   id: process.env.GOVFAQID,
   gid: process.env.GOVFAQGID,
 };
 
-const govFaqsURL = `https://docs.google.com/spreadsheets/d/e/${govFaqRefs.id}/pub?gid=${govFaqRefs.gid}&single=true&output=csv`;
+const govFaqsURL = baseFaqsURL(govFaqRefs.id, govFaqRefs.gid);
 
 module.exports = {
   baseURL,
