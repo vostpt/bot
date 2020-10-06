56189bb223be26e9b8446c16b9054b03f57f9005
Miguel Carreiro <miguel.carreiro97@gmail.com>
2020-10-06 09:02:29 +0100

    feat: Update sequelize version (v5 to v6)

--- src/database/migrations/20200325221548-update-corona-reports.js
+++ src/database/migrations/20200325221548-update-corona-reports.js
@@ -1,6 +1,6 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.addColumn('CoronaReports', 'md5sum', {
+  up: async (queryInterface, Sequelize) => queryInterface.addColumn('CoronaReports', 'md5sum', {
     type: Sequelize.DataTypes.CHAR(32),
   }),
-  down: queryInterface => queryInterface.removeColumn('CoronaReports', 'md5sum'),
+  down: async queryInterface => queryInterface.removeColumn('CoronaReports', 'md5sum'),
 };
--- src/database/models/coronareports.js
+++ src/database/models/coronareports.js
@@ -1,9 +1,17 @@
+const {
+  Model,
+} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  const CoronaReports = sequelize.define('CoronaReports', {
+  class CoronaReports extends Model {}
+
+  CoronaReports.init({
     title: DataTypes.TEXT,
     link: DataTypes.TEXT,
     md5sum: DataTypes.CHAR(32),
-  }, {});
-
+  }, {
+    sequelize,
+    modelName: 'CoronaReports',
+  });
   return CoronaReports;
 };
