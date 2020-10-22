4c8cf766fa27a3f90cbf073b0ecb5b5a385eedf5
Tomahock <eu@tomahock.com>
2020-10-22 09:47:42 +0100

    weather warnings to telegram

---
+++ config/telegram.js
@@ -0,0 +1,14 @@
+const {
+    TELEGRAM_API_KEY,
+    TELEGRAM_CHAT_ID,
+  } = process.env;
+  
+  const telegramKeys = [{
+      key: TELEGRAM_API_KEY,
+      chat_id: TELEGRAM_CHAT_ID
+  }];
+  
+  module.exports = {
+    telegramKeys,
+  };
+  
