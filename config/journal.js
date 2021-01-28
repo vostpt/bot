5bbcfbaf4ae0cb4baec7305f2a3e04adb04d839c
Miguel Carreiro <miguel.carreiro97@gmail.com>
2021-01-28 11:47:29 +0000

    feat: Add index C from serie II "despachos"

--- config/journal.js
+++ config/journal.js
@@ -1,5 +1,7 @@
 const journalSerieI = 'https://dre.pt/home/-/dre/feed/exclusive/1/cacheLevelPage?serie=I';
 
+const journalSerieIIGov = 'https://dre.pt/home/-/dre/feed/exclusive/1/cacheLevelPage?serie=II&parte=C';
+
 const journalURLStruct = {
   prefix: 'https://dre.pt/web/guest/home/-/dre/',
   suffix: '/details/maximized',
@@ -7,5 +9,6 @@ const journalURLStruct = {
 
 module.exports = {
   journalSerieI,
+  journalSerieIIGov,
   journalURLStruct,
 };
--- src/api/Journal.js
+++ src/api/Journal.js
@@ -2,9 +2,17 @@ const Parser = require('rss-parser');
 
 const parser = new Parser();
 
-const { journalSerieI } = require('../../config/journal');
+const { journalSerieI, journalSerieIIGov } = require('../../config/journal');
 
-const getJournal = async () => parser.parseURL(journalSerieI).then(result => result.items);
+const getJournal = async () => {
+  const serieI = await parser.parseURL(journalSerieI).then(result => result.items);
+
+  const serieIIGov = await parser.parseURL(journalSerieIIGov).then(result => result.items);
+
+  const serieIIGovFilt = serieIIGov.filter(decree => decree.title.includes('Despacho'));
+
+  return [...serieI, ...serieIIGovFilt];
+};
 
 module.exports = {
   getJournal,
