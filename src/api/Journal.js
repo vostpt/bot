9e7c6b3788ea399979882c5b0d9dfd07d5187ada
Miguel Carreiro <miguel.carreiro97@gmail.com>
2021-02-10 12:01:41 +0000

    fix: Tweet only 1st serie decrees

--- src/api/Journal.js
+++ src/api/Journal.js
@@ -5,11 +5,13 @@ const parser = new Parser();
 const { journalSerieI, journalSerieIIGov } = require('../../config/journal');
 
 const getJournal = async () => {
-  const serieI = await parser.parseURL(journalSerieI).then(result => result.items);
+  const serieI = await parser.parseURL(journalSerieI)
+    .then(result => result.items.map(item => ({ ...item, serie: 1 })));
 
   const serieIIGov = await parser.parseURL(journalSerieIIGov).then(result => result.items);
 
-  const serieIIGovFilt = serieIIGov.filter(decree => decree.title.includes('Despacho'));
+  const serieIIGovFilt = serieIIGov.filter(decree => decree.title.includes('Despacho'))
+    .map(item => ({ ...item, serie: 2 }));
 
   return [...serieIIGovFilt, ...serieI];
 };
