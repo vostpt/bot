0b6b596ed6a881e4c1a00298ce08431e11cdc0bd
Miguel Carreiro <miguel.carreiro97@gmail.com>
2021-03-19 00:30:42 +0000

    fix: Refactor Mastodon service, to allow posts to Pleroma

--- config/mastodon.js
+++ config/mastodon.js
@@ -1,8 +1,18 @@
-const accessToken = process.env.MASTODON_ACCESS_TOKEN;
+const mastodonURL = 'https://masto.pt/api/v1';
 
-const mastodonURL = 'https://masto.pt/api/v1/';
+const pleromaURL = 'https://pleroma.pt/api/v1';
+
+const mastodonKeys = {
+  main: {
+    access_token: process.env.MASTODON_ACCESS_TOKEN,
+    api_url: mastodonURL,
+  },
+  dre: {
+    access_token: process.env.PTDRE_PLEROMA_ACCESS_TOKEN,
+    api_url: pleromaURL,
+  },
+};
 
 module.exports = {
-  accessToken,
-  mastodonURL,
+  mastodonKeys,
 };
